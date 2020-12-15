11fb7c66ac0328ec1df2fdc919bdea290493dec8
Luka Spahija <spahundula@gmail.com>
2020-12-15 20:28:46 +0100

    added server.js

---
+++ singleproxyweb/server.js
@@ -0,0 +1,40 @@
+const { parse } = require('url')
+const next = require('next')
+const express = require('express')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({dev})
+const handle = app.getRequestHandler()
+
+const port = dev ? 3000 : 443
+const https = require('https')
+const http = require('http')
+const fs = require('fs');
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/alealogic.com/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/alealogic.com/cert.pem')
+};
+
+
+app.prepare().then(() => {
+    const httpApp = express();
+    httpApp.get("*", (req, res) => {
+        res.writeHead(302, {
+            Location: "https://" + req.headers.host + req.url
+        });
+        res.end();
+    });
+
+    http.createServer(httpApp).listen(80, err => {
+        if (err) throw err;
+        console.log(`> HTTP Ready on http://localhost:${80}`);
+    });
+
+    https.createServer(options, (req, res) => {
+        const parsedUrl = parse(req.url, true)
+        handle(req, res, parsedUrl)
+    }).listen(port, (err) => {
+        if (err) throw err
+        console.log(`> Ready on http://localhost:${port}`)
+    })
+});
