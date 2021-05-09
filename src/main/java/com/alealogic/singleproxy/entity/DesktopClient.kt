f30a5d7c35a702fa1deffade18b7c1e219642cae
Luka Spahija <lspahijaoec@gmail.com>
2021-05-09 14:27:46 -0500

    using data class for equals implementation

--- src/main/java/com/alealogic/singleproxy/entity/DesktopClient.kt
+++ src/main/java/com/alealogic/singleproxy/entity/DesktopClient.kt
@@ -7,21 +7,21 @@ import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
-class DesktopClient {
+data class DesktopClient (
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long? = null
-    var key: String? = null
-    var customerId: Long? = null
-    var registered = false
-    var releaseId: Long? = null
-    var lastHeartbeat: LocalDateTime? = null
-    var lastIp: String? = null
-    var ldflags: String? = null
-    var updateToReleaseId: Long? = null
-    var obsolete: Boolean = false
-    var updateInitiatorDesktopClientId: Long? = null
-    var ipId: String? = null
-    var port: Int? = null
+    var id: Long? = null,
+    var key: String? = null,
+    var customerId: Long? = null,
+    var registered: Boolean = false,
+    var releaseId: Long? = null,
+    var lastHeartbeat: LocalDateTime? = null,
+    var lastIp: String? = null,
+    var ldflags: String? = null,
+    var updateToReleaseId: Long? = null,
+    var obsolete: Boolean = false,
+    var updateInitiatorDesktopClientId: Long? = null,
+    var ipId: String? = null,
+    var port: Int? = null,
     var active: Boolean = true
-}
+)
